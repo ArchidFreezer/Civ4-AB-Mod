c22649eb543b89d439c9be9690a401ed0061d561
Archid <git@archid.org>
2023-06-20 17:15:58 +0100

    Set XML HotKeyPriority default value
    The HotKeyPriority value is used across many XML entity definition files with 0 being the most common value. Setting this as the default when reading the files allows this tag to be removed from many XML files.
    
    Resolves: AB4-3

--- CvGameCoreDLL/CvInfos.cpp
+++ CvGameCoreDLL/CvInfos.cpp
@@ -268,7 +268,7 @@ bool CvHotkeyInfo::read(CvXMLLoadUtility* pXML) {
 	if (pXML->GetChildXmlValByName(&iVal, "iHotKeyPriority")) {
 		setHotKeyPriority(iVal);
 	} else {
-		setHotKeyPriority(-1);
+		setHotKeyPriority(0);
 	}
 
 	if (pXML->GetChildXmlValByName(szTextVal, "HotKeyAlt")) {
