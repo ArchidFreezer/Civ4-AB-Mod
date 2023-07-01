f4af835725c9ca5eb26632c31ad7e3103d225acc
Archid <git@archid.org>
2023-07-01 11:38:46 +0100

    Prevent range bound units from grouping with better ranged units
    When units group the effective range type of the group is that of the most restrictive range limit. In the early game especially when there are unit types with very restrictive range types this can prevent some units from performing their function. Settlers may be prevented from leaving their territory to settle or armies being able to reach other civs. This change should resolves this type of issue.
    
    AB4-8

--- CvGameCoreDLL/CvUnitAI.cpp
+++ CvGameCoreDLL/CvUnitAI.cpp
@@ -17761,6 +17761,18 @@ bool CvUnitAI::AI_canGroupWithAIType(UnitAITypes eUnitAI) const {
 bool CvUnitAI::AI_allowGroup(const CvUnit* pUnit, UnitAITypes eUnitAI) const {
 	CvSelectionGroup* pGroup = pUnit->getGroup();
 	CvPlot* pPlot = pUnit->plot();
+	UnitRangeTypes eOurUnitRange = getRangeType();
+	UnitRangeTypes eTheirUnitRange = pUnit->getRangeType();
+
+	// Never group if we are home bound
+	if (eOurUnitRange == UNITRANGE_HOME) {
+		return false;
+	}
+
+	// Don't group with units with better range
+	if (eOurUnitRange < eTheirUnitRange) {
+		return false;
+	}
 
 	if (pUnit == this) {
 		return false;
@@ -17807,7 +17819,7 @@ bool CvUnitAI::AI_allowGroup(const CvUnit* pUnit, UnitAITypes eUnitAI) const {
 	}
 
 	if (eUnitAI == UNITAI_SETTLE) {
-		if (GET_PLAYER(getOwnerINLINE()).AI_getAnyPlotDanger(pPlot, 3)) {
+		if (eOurUnitRange < UNITRANGE_RANGE || GET_PLAYER(getOwnerINLINE()).AI_getAnyPlotDanger(pPlot, 3)) {
 			return false;
 		}
 	} else if (eUnitAI == UNITAI_ASSAULT_SEA) {
