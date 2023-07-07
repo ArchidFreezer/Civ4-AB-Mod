a05cf64831ba2d8bf8e819d483c9c397dd0e1d66
Archid <git@archid.org>
2023-07-07 11:54:06 +0100

    Add tech tree from Rise of Mankind
    The RoM tech tree is greatly expanded with many new techs added. This commit adds those techs, with some tweaks, into the game updating building, civic, event and era files as required.
    
    Resources such as buildings, bonus, units, etc. have not been moved to new techs in this commit, only those that required references to be updated.
    
    AB4-207

--- Support/Tools/export-techtree.bat
+++ Support/Tools/export-techtree.bat
@@ -1,1 +1,3 @@
-java -cp civ4utils.jar org.archid.civ4.techinfo.TechUtils -x -f "../../../Assets/Xml/Technologies/Civ4TechInfos.xml" -o "../../Docs/"
+set JAVA_HOME=E:\Apps\Java\jdk-8.0.341
+set Path=%JAVA_HOME%\bin;%PATH%
+java -cp civ4utils.jar org.archid.civ4.main.InfoUtils -f "E:/WinMyDocs/My Games/Beyond the Sword/MODS/A-Mod/Assets/Xml/Technologies/Civ4TechInfos.xml" -x "../Docs/Civ4Archid.xlsx" -t Tech -e
