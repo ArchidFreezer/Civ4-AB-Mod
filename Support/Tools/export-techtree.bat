03686d938071645ab56a8189513005c1bb206843
Archid <git@archid.org>
2023-07-07 09:49:54 +0100

    Add java tooling batch files
    Added some batch files for common tooling tasks
    
    AB4-147

---
+++ Support/Tools/export-techtree.bat
@@ -0,0 +1,1 @@
+java -cp civ4utils.jar org.archid.civ4.techinfo.TechUtils -x -f "../../../Assets/Xml/Technologies/Civ4TechInfos.xml" -o "../../Docs/"
---
+++ Support/Tools/import-all.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=E:\Apps\Java\jdk-8.0.341
+set Path=%JAVA_HOME%\bin;%PATH%
+java -cp civ4utils.jar org.archid.civ4.main.InfoUtils -f "./output" -x "../Docs/Civ4Archid.xlsx" -t all -i 
