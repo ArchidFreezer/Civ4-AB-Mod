4e60b024763859d0926a0eca6bc66088eb98d265
Archid <git@archid.org>
2023-07-15 12:02:48 +0100

    Empty output folder before processing export/import of buildings
    The default behaviour of the java tooling is to create backups of any existing files, we don't need that as we are not creating the output into the game folders so clear up the output folder first and the extra xlsx files
    
    AB4-147

--- Support/Tools/export-import-buildings.bat
+++ Support/Tools/export-import-buildings.bat
@@ -1,6 +1,8 @@
 set JAVA_HOME=E:\Apps\Java\jdk-8.0.341
 set Path=%JAVA_HOME%\bin;%PATH%
+@PushD "./output" 2> NUL && (RD /S /Q "." 2> NUL & PopD)
 java -cp civ4utils.jar org.archid.civ4.main.InfoUtils -f "E:/WinMyDocs/My Games/Beyond the Sword/MODS/A-Mod/Assets/Xml/Buildings/Civ4BuildingInfos.xml" -x "../Docs/Civ4Archid.xlsx" -t Building -e
 java -cp civ4utils.jar org.archid.civ4.main.InfoUtils -f "E:/WinMyDocs/My Games/Beyond the Sword/MODS/A-Mod/Assets/Xml/Buildings/Civ4BuildingClassInfos.xml" -x "../Docs/Civ4Archid.xlsx" -t BuildingClass -e
 java -cp civ4utils.jar org.archid.civ4.main.InfoUtils -f "./output" -x "../Docs/Civ4Archid.xlsx" -t Building -i 
 java -cp civ4utils.jar org.archid.civ4.main.InfoUtils -f "./output" -x "../Docs/Civ4Archid.xlsx" -t BuildingClass -i 
+del /q "../Docs/Civ4Archid.xlsx.2*"
